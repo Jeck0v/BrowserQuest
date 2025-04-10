829305a89593a95e7437b49481ede04195ac5160
Jeck0v <arnaud.fischer75@gmail.com>
2025-04-11 01:57:21 +0200

    je suis l'homme le plus heureux du monde, comme quoi il faut toujours persévérer

--- browserquest-infra/scripts/start-minikube.bat
+++ browserquest-infra/scripts/start-minikube.bat
@@ -2,3 +2,4 @@
 minikube delete
 minikube start --driver=docker --feature-gates=IPv6DualStack=true --network-plugin=cni --cni=bridge --extra-config=kubelet.node-ip=::1
 minikube addons enable ingress
+minikube addons enable metrics-server
